6e2c95d0d3ac566f44904caa4534c20919438936
mohsen <69722249+mohsnabc@users.noreply.github.com>
2026-07-17 17:21:00 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
+from telegram import Update
 import os
 import threading
 from flask import Flask
-from telegram.ext import Application, CommandHandler
+from telegram.ext import Application, CommandHandler, MessageHandler, ContextTypes, filters
 
 TOKEN = os.getenv("TOKEN")
 
@@ -11,12 +12,21 @@ app = Flask(__name__)
 def home():
     return "OK"
 
-async def start(update, context):
-    await update.message.reply_text("سلام 👋 ربات فعال است")
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("سلام 👋 لینک اینستاگرام را بفرست")
+
+async def check_link(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text
+
+    if "instagram.com" in text:
+        await update.message.reply_text("✅ لینک اینستاگرام دریافت شد")
 
 def run_bot():
     bot = Application.builder().token(TOKEN).build()
+
     bot.add_handler(CommandHandler("start", start))
+    bot.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, check_link))
+
     bot.run_polling(stop_signals=None)
 
 threading.Thread(target=run_bot).start()
