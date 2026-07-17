d8926cb46ae0586c68c6f83f7167f6b8052df898
mohsen <69722249+mohsnabc@users.noreply.github.com>
2026-07-17 16:39:37 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,16 @@
+import os
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
 
+TOKEN = os.getenv("TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("سلام 👋 ربات فعال شد")
+
+app = Application.builder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+print("Bot is running...")
+
+app.run_polling()
